7614d0ac71656ff43a4ac0a44e3f578d3ca67634
Zemile30 <zemile.nesirli@gmail.com>
2025-05-19 17:47:55 +0000

    firstcommit

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from .routes import get_news
+
+app = Flask(__name__)
+
+CORS(app, resources = {r"/api/news": {
+    "origins": ["https://zemile30.github.io"],
+    "methods": ["GET"]
+}})
+
+@app.route("/api/news", methods=["GET"])
+def news_route():
+    return get_news()
+    
