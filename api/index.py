e58478abcdeb85b9c4ecdd0d31148bbcc882b8f9
Zemile30 <zemile.nesirli@gmail.com>
2025-05-21 14:11:48 +0000

    changed

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from .routes import get_news
 
 app = Flask(__name__)
 
-CORS(app, resources = {r"/api/news": {
+CORS(app, resources={r"/api/news": {
     "origins": ["https://zemile30.github.io"],
     "methods": ["GET"]
 }})
